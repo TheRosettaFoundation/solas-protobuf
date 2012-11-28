be95eab329167feb15c0ca575fcb28aa18487cb7
David O Carroll <spaceindaver0@gmail.com>
2012-11-28 13:44:44 +0000

    Modify shell scripts to compile model classes

--- cpp_generate_protos.sh
+++ cpp_generate_protos.sh
@@ -1,1 +1,5 @@
-protoc --cpp_out=./generated/cpp/ -I./protos/emails ./protos/emails/*.proto
+### Generate Emails ###
+protoc --cpp_out=./generated/cpp/emails/ -I./protos/emails ./protos/emails/*.proto
+
+### Generate Model Classes ###
+protoc --cpp_out=./generated/cpp/models/ -I./protos/models ./protos/models/*.proto
--- php_generate_protos.sh
+++ php_generate_protos.sh
@@ -1,9 +1,26 @@
-protoc-gen-php -o ./generated/php -i ./protos/emails ./protos/emails/EmailMessage.proto
-protoc-gen-php -o ./generated/php -i ./protos/emails ./protos/emails/OrgMembershipAccepted.proto
-protoc-gen-php -o ./generated/php -i ./protos/emails ./protos/emails/OrgMembershipRefused.proto
-protoc-gen-php -o ./generated/php -i ./protos/emails ./protos/emails/PasswordResetEmail.proto
-protoc-gen-php -o ./generated/php -i ./protos/emails ./protos/emails/TaskArchived.proto
-protoc-gen-php -o ./generated/php -i ./protos/emails ./protos/emails/TaskClaimed.proto
-protoc-gen-php -o ./generated/php -i ./protos/emails ./protos/emails/TaskScoreEmail.proto
-protoc-gen-php -o ./generated/php -i ./protos/emails ./protos/emails/TaskTranslationUploaded.proto
-protoc-gen-php -o ./generated/php -i ./protos/emails ./protos/emails/UserTaskClaim.proto
+### Generate Email Classes ###
+protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/EmailMessage.proto
+protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/OrgMembershipAccepted.proto
+protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/OrgMembershipRefused.proto
+protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/PasswordResetEmail.proto
+protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/TaskArchived.proto
+protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/TaskClaimed.proto
+protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/TaskScoreEmail.proto
+protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/TaskTranslationUploaded.proto
+protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/UserTaskClaim.proto
+
+### Generate Model Classes ###
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/ArchivedTask.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/Badge.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/Country.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/Language.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/Login.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/MembershipRequest.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/Organisation.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/PasswordReset.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/PasswordResetRequest.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/Register.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/Tag.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/TaskMetadata.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/Task.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/User.proto
