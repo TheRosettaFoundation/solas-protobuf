b6962bb87f205f6671029a39b2ba95ba582bd224
David O Carroll <spaceindaver0@gmail.com>
2013-01-31 17:17:37 +0000

    Add workflowNode and graph protos

--- php_generate_protos.sh
+++ php_generate_protos.sh
@@ -25,6 +25,8 @@ protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/Tag.
 protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/TaskMetadata.proto
 protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/Task.proto
 protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/User.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/WorkflowNode.proto
+protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/WorkflowGraph.proto
 
 ### Generate Requests Classes ###
 protoc-gen-php -o ./generated/php/requests -i ./protos/requests ./protos/requests/UserTaskScoreRequest.proto
