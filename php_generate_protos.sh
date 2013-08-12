79be0b02f028e251e40e34ad26f16d1f5d19e1ad
David O Carroll <spaceindaver0@gmail.com>
2013-08-12 12:11:52 +0100

    Add proto for UserUnclaimedTaskEmail

--- php_generate_protos.sh
+++ php_generate_protos.sh
@@ -18,6 +18,7 @@ protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/Clai
 protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/ClaimedTaskUploaded.proto
 protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/UserReferenceEmail.proto
 protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/UserBadgeAwardedEmail.proto
+protoc-gen-php -o ./generated/php/emails -i ./protos/emails ./protos/emails/UserUnclaimedTaskEmail.proto
 
 ### Generate Model Classes ###
 protoc-gen-php -o ./generated/php/models -i ./protos/models ./protos/models/ArchivedTask.proto
